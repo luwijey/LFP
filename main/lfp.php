dcf8f933ec67b103eb354881f0346b49e00e6d00
Louie Jay Castillo <castillo.louiejay@gmail.com>
2025-09-03 02:25:53 +0800

    okay na muna siguro to

--- main/lfp.php
+++ main/lfp.php
@@ -12,27 +12,32 @@ include '../components/header.php';
     <title>L&F Platform</title>
 </head>
 <body>
-<div class="container-fluid custom-header">
-   <nav class="navbar p-3 mb-3">
-        <a class= "navbar-brand custom-logo" href="../main/lfp.php">
-            <img src="../uploads/circle_logo.png" style="width:45px; height:auto; margin-right: 10px;" alt="logo">
-            Lost & Found  
-        </a>    
-        <ul class="nav nav-pills custom-list " id="pills-tab" role="tablist">
-            <li class="nav-item " role="presentation">
-                <button class="nav-link active" id="pills-dashboard-tab" data-bs-toggle="pill" data-bs-target="#pills-dashboard" type="button" role="tab" aria-controls="pills-dashboard" aria-selected="true">Dashboard</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-reports-tab" data-bs-toggle="pill" data-bs-target="#pills-reports" type="button" role="tab" aria-controls="pills-reports" aria-selected="false">Reports</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
-            </li>
-            <li class="nav-item">
-                <button class="btn" id="logout" type="button">Log Out</button>
-            </li>
-        </ul>       
-   </nav>
-</div>
+    <div class="container-fluid custom-header">
+        <nav class="navbar p-1.5 mb-3">
+            <a class= "navbar-brand custom-logo" tabindex="-1" href="../main/lfp.php">
+                <img src="../uploads/circle_logo.png" style="width:45px; height:auto; margin-right: 10px;" alt="logo">
+                Lost & Found  
+            </a>    
+            <ul class="nav nav-pills gap-1 custom-list " id="pills-tab" role="tablist">
+                <li class="nav-item " role="presentation">
+                    <button class="nav-link active" id="pills-dashboard-tab" data-bs-toggle="pill" data-bs-target="#pills-dashboard" type="button" role="tab" aria-controls="pills-dashboard" aria-selected="true">Dashboard</button>
+                </li>
+
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-reports-tab" data-bs-toggle="pill" data-bs-target="#pills-reports" type="button" role="tab" aria-controls="pills-reports" aria-selected="false">Reports</button>
+                </li>
+
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+                </li>
+
+                <li class="nav-item">
+                    <button class="btn" id="logout" type="button">Log Out</button>
+                </li>
+            </ul>       
+        </nav>
+    </div>
+    <main>
+    </main>
 </body>
 </html>
